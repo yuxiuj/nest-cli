617225c3ef7d12cdc6726d89a999de70eaea751f
yuxiu <liboysu@163.com>
2019-12-30 14:20:35 +0800

    feat: 代码格式化

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { Repository } from "typeorm";
+import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../db/user.entity';
 import { HttpException, HttpStatus } from '@nestjs/common';
